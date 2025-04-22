5194cb5ac871f1c75da8831043e16023f0c09839
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-04-22 20:45:58 +0800

    update styles for root-container and Footer component for improved aesthetics

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#16181c] h-[200px]">
-      <div className="max-w-[1140px] mx-auto py-3 sm:px-10 md:px-16 text-white font-poppins">
-        <p className="text-sm text-slate-500">
+    <footer className="bg-[#1f2128] h-[200px]">
+      <div className="max-w-[1140px] mx-auto py-3 sm:px-10 md:px-16 font-poppins">
+        <p className="text-sm text-slate-400">
           &copy; PlayStack | Powered by{" "}
-          <a href="https://rawg.io/" target="_blank" className="text-slate-400 hover:text-slate-300">
+          <a href="https://rawg.io/" target="_blank" className="text-slate-300 hover:text-slate-100">
             RAWG
           </a>
         </p>
