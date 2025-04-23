6b351d3595dfe0271cb886056afe576c4fadbca0
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-04-23 14:04:54 +0800

    refactor fetchGames function for improved readability and maintainability

--- lib/actions/game.ts
+++ lib/actions/game.ts
@@ -19,10 +19,14 @@ export const fetchGames = async (params: GameQueryParams = {}) => {
     if (params.search) queryParams.append("search", params.search);
     if (params.page) queryParams.append("page", params.page);
     if (params.page_size) queryParams.append("page_size", params.page_size);
-    if (params.search_precise) queryParams.append("search_precise", params.search_precise);
-    if (params.search_exact) queryParams.append("search_exact", params.search_exact);
+    if (params.search_precise)
+      queryParams.append("search_precise", params.search_precise);
+    if (params.search_exact)
+      queryParams.append("search_exact", params.search_exact);
 
-    const apiUrl = `https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&${queryParams.toString()}`;
+    const apiUrl = `https://api.rawg.io/api/games?key=${
+      process.env.RAWG_API_KEY
+    }&${queryParams.toString()}`;
 
     const response = await fetch(apiUrl, {
       method: "GET",
@@ -39,6 +43,29 @@ export const fetchGames = async (params: GameQueryParams = {}) => {
     return data;
   } catch (error) {
     console.log("Error fetching games:", error);
+    throw error;
+  }
+};
+
+export const getGameDetails = async (slug: string) => {
+  try {
+    const apiUrl = `https://api.rawg.io/api/games/${slug}?key=${process.env.RAWG_API_KEY}`;
+
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`RAWG API responded with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error fetching game details:", error);
     throw error;
   }
 };
