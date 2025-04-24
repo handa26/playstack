6811d9b83a03330af1f8ae8b4977f3ce5c204764
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-04-24 19:41:15 +0800

    add formattedDate function for improved date formatting options

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 export const formatDate = (date: Date) => {
   return date.toISOString().split("T")[0];
 };
+
+export const formattedDate = (date: string) => {
+  const options: Intl.DateTimeFormatOptions = { 
+    month: 'short', 
+    day: 'numeric', 
+    year: 'numeric' 
+  };
+
+  return new Date(date).toLocaleDateString("en-US", options);
+};
