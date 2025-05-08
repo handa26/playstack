3a6cca122912f0ade586d602ef0b7ac05ca7b746
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-08 10:09:15 +0800

    feat: implement dynamic tab handling in CustomTabs component

--- components/CustomTabs.tsx
+++ components/CustomTabs.tsx
@@ -1,10 +1,33 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const CustomTabs = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const status = params.get("status") || "played";
+
+  const [activeTab, setActiveTab] = useState(status);
+
+  useEffect(() => {
+    setActiveTab(status);
+  }, [status]);
+
+  const handleTabChange = (newValue: string) => {
+    setActiveTab(newValue);
+    router.push(`/profile/games?status=${newValue}`, { scroll: false });
+  };
+
   return (
     <>
-      <Tabs defaultValue="played" className="max-w-xs w-full">
+      <Tabs
+        value={activeTab}
+        onValueChange={handleTabChange}
+        className="max-w-xs w-full"
+      >
         <TabsList className="p-0 h-auto bg-background gap-1">
           <TabsTrigger
             className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
