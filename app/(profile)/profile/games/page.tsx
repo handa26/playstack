a396e706e80983a50f6650910080ac677fd96db4
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-11 19:20:09 +0800

    refactor: remove Suspense wrapper from CustomTabs for improved performance

--- app/(profile)/profile/games/page.tsx
+++ app/(profile)/profile/games/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
+import { Suspense } from "react";
+
 import CustomTabs from "@/components/CustomTabs";
 
 const Page = () => {
   return (
-    <>
+    <Suspense>
       <CustomTabs />
-    </>
+    </Suspense>
   );
 };
 
--- components/CustomTabs.tsx
+++ components/CustomTabs.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -28,52 +28,50 @@ const CustomTabs = () => {
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Tabs
-          value={activeTab}
-          onValueChange={handleTabChange}
-          className="w-full"
-        >
-          <TabsList className="p-0 h-auto bg-background gap-1">
-            <TabsTrigger
-              className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
-              value="played"
-            >
-              Played
-            </TabsTrigger>
-            <TabsTrigger
-              className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
-              value="playing"
-            >
-              Playing
-            </TabsTrigger>
-            <TabsTrigger
-              className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
-              value="backlog"
-            >
-              Backlog
-            </TabsTrigger>
-            <TabsTrigger
-              className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
-              value="wishlist"
-            >
-              Wishlist
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="played">
-            <GameList games={gamesStorage.played} />
-          </TabsContent>
-          <TabsContent value="playing">
-            <GameList games={gamesStorage.playing} />
-          </TabsContent>
-          <TabsContent value="backlog" className="">
-            <GameList games={gamesStorage.backlog} />
-          </TabsContent>
-          <TabsContent value="wishlist">
-            <GameList games={gamesStorage.wishlist} />
-          </TabsContent>
-        </Tabs>
-      </Suspense>
+      <Tabs
+        value={activeTab}
+        onValueChange={handleTabChange}
+        className="w-full"
+      >
+        <TabsList className="p-0 h-auto bg-background gap-1">
+          <TabsTrigger
+            className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
+            value="played"
+          >
+            Played
+          </TabsTrigger>
+          <TabsTrigger
+            className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
+            value="playing"
+          >
+            Playing
+          </TabsTrigger>
+          <TabsTrigger
+            className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
+            value="backlog"
+          >
+            Backlog
+          </TabsTrigger>
+          <TabsTrigger
+            className="data-[state=active]:bg-slate-600 data-[state=active]:text-primary-foreground text-slate-500 cursor-pointer"
+            value="wishlist"
+          >
+            Wishlist
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="played">
+          <GameList games={gamesStorage.played} />
+        </TabsContent>
+        <TabsContent value="playing">
+          <GameList games={gamesStorage.playing} />
+        </TabsContent>
+        <TabsContent value="backlog" className="">
+          <GameList games={gamesStorage.backlog} />
+        </TabsContent>
+        <TabsContent value="wishlist">
+          <GameList games={gamesStorage.wishlist} />
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
