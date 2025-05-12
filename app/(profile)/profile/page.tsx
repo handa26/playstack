f2aa52604c482bd532e4d4c143d0febe0bdc300f
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-12 10:02:59 +0800

    refactor: update game storage retrieval to use state and add optional chaining for safety

--- app/(profile)/profile/page.tsx
+++ app/(profile)/profile/page.tsx
@@ -1,27 +1,35 @@
 "use client";
+import { useState, useEffect } from "react";
 import { Separator } from "@/components/ui/separator";
 
 import { getGamesFromStorage } from "@/lib/storage";
 import { threeDigitsNum } from "@/lib/utils";
 
 const Page = () => {
-  const { backlog, playing, wishlist, played } = getGamesFromStorage();
+  const [gamesStorage, setGamesStorage] = useState<GameStorage>([]);
 
-  const totalGames = backlog.length + playing.length + wishlist.length + played.length;
+  useEffect(() => {
+    const localStorageGames = getGamesFromStorage();
+    setGamesStorage(localStorageGames);
+  }, []);
+
+  const { backlog, playing, wishlist, played } = gamesStorage;
+
+  const totalGames = backlog?.length + playing?.length + wishlist?.length + played?.length;
 
   return (
     <div className="my-2">
       <div className="flex justify-center gap-20">
         <div className="text-center">
-          <h2 className="text-8xl">{threeDigitsNum(totalGames)}</h2>
+          <h2 className="text-8xl">{threeDigitsNum(totalGames) || 0}</h2>
           <p className="text-[16px]">Total Games</p>
         </div>
         <div className="text-center">
-          <h2 className="text-8xl">{threeDigitsNum(played.length)}</h2>
+          <h2 className="text-8xl">{threeDigitsNum(played?.length) || 0}</h2>
           <p className="text-[16px]">Total Games Played</p>
         </div>
         <div className="text-center">
-          <h2 className="text-8xl">{threeDigitsNum(backlog.length)}</h2>
+          <h2 className="text-8xl">{threeDigitsNum(backlog?.length) || 0}</h2>
           <p className="text-[16px]">Backlog Games</p>
         </div>
       </div>
