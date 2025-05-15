db89e388e532e0c994cc581a779305142a1928f1
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-15 10:10:40 +0800

    refactor: update layout and styling of GameCard and Footer components for improved responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-[#1f2128] h-[200px]">
-      <div className="max-w-[1140px] mx-auto py-3 sm:px-10 md:px-16 font-poppins">
+      <div className="max-w-[1140px] mx-auto py-3 px-3 md:px-16 font-poppins">
         <p className="text-sm text-slate-400">
           &copy; PlayStack | Powered by{" "}
           <a href="https://rawg.io/" target="_blank" className="text-slate-300 hover:text-slate-100">
--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -10,13 +10,13 @@ interface GameCardProps {
 
 const GameCard = ({ name, background_image, slug }: GameCardProps) => {
   return (
-    <Link href={`/games/${slug}`} className="w-[170px] h-[230px] relative group">
+    <Link href={`/games/${slug}`} className="w-[100%] h-[250px] relative group md:w-[170px] md:h-[230px]">
       <Image
         src={background_image || "/images/eldenring-cover.jpg"}
         alt={`${name} cover`}
         width={0}
         height={0}
-        className="rounded-lg shadow-lg transition-transform duration-200 group-hover:brightness-50 w-[170px] h-[230px] object-cover"
+        className="rounded-lg shadow-lg transition-transform duration-200 group-hover:brightness-50 w-[100%] h-[250px] object-cover md:w-[170px] md:h-[230px]"
         sizes="(max-width: 768px) 100vw, 50vw"
       />
       <p className="absolute font-poppins font-semibold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-10">
